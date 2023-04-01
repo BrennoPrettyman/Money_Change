61cd2e0fa21e2110ad6ae6f373f518587cc9d839
Brenno Prettyman <brennoprettyman@gmail.com>
2023-04-01 17:52:41 -0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Money_Change;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("Digite o Preço: ");
+        Console.Write("R$ ");
+        decimal compra = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Digite o Preço que irá pagar: ");
+        Console.Write("R$ ");
+        decimal venda = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine();
+
+        decimal Troco = compra - venda;
+
+        Console.WriteLine("Seu troco irá ser: ");
+        Console.WriteLine($"R$: {Troco}");
+
+
+    }
+}
